7c941588cb6e8c2c3d0ecb4d7caa225d84cc699e
ashishkhuraishy <ashishkhuraishy@gmail.com>
2021-08-19 20:09:37 +0530

    initial commit

---
+++ Lox.java
@@ -0,0 +1,11 @@
+/**
+ * Lox
+ */
+public class Lox {
+
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            System.out.println("Usage : jlox [script]");
+        }
+    }
+}
