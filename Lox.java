3dd42c090e43668b2e324bfc64cc25b015663390
ashishkhuraishy <ashishkhuraishy@gmail.com>
2021-08-19 20:34:25 +0530

    base setup

--- Lox.java
+++ Lox.java
@@ -1,11 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Lox
  */
 public class Lox {
 
-    public static void main(String[] args) {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage : jlox [script]");
+            System.exit(64);
+        }else if (args.length == 1) {
+            System.out.println(args[1]);
+            runFile(args[1]);
+        }else{
+            runPrompt();
         }
     }
+
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
+            
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null ||line.equals("exit()")) {
+                System.out.println("exiting...");
+                return;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+
+    }
+
+
+    public static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %d] Error %s : %s\n", line, where, message);
+        hadError = true;       
+    }
+
 }
---
+++ Scanner.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    final String source;
+
+    Scanner(String source){
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        List<Token> tokens = new ArrayList<Token>();
+
+        return tokens;
+    }
+
+}
